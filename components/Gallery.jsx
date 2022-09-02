5ae93ffea669bd5f56ae0c7edfcbedbec79791bd
Irene Wang <ireneziwang@gmail.com>
2022-09-02 13:58:24 -0500

    url of 10 dog images will render

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -4,16 +4,11 @@ import { Text, View } from 'react-native';
 const Gallery = ({tenDogs}) => {
   return (
     <View>
+      {/* <Text>Hi {tenDogs}</Text> */}
       <Text>Hi {tenDogs.map(dog => (<Text>{dog}</Text>))}</Text>
     </View>
   );
 }
 
-export default const Image = () => {
-  return (
-    <View></View>
-  )
-}
 export default Gallery;
-
 
