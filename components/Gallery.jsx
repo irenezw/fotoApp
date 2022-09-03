cb4b00089b520ea77ada00a96b21d94915780f85
Irene Wang <ireneziwang@gmail.com>
2022-09-03 14:12:21 -0500

    like button upon pressing will console.log 'LIKED'

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, FlatList, ImageBackground, ScrollView, TouchableOpacity} from 'react-native';
+import { Text, View, FlatList, ImageBackground, ScrollView, TouchableOpacity, Button} from 'react-native';
 //TODO diff-ing algorithm that compares previous render with the current code
 //in order for diffing to occur in arrays. react wants us to use KEY PROPS
 //bc it looks at key to determine if list item has changed
@@ -7,29 +7,20 @@ import { Text, View, FlatList, ImageBackground, ScrollView, TouchableOpacity} fr
 const Gallery = ({tenDogs}) => {
   return (
     <ScrollView>
-      {/* {console.log(`"${tenDogs[0]}"`)} */}
       {tenDogs.map((dog, i) => (
-        <TouchableOpacity onPress={()=>{console.log('LIKE')}}>
+        <TouchableOpacity>
         <ImageBackground
           key={i}
           style={{width: 200, height: 200}}
-          source = {{uri: dog}}
-        ></ImageBackground>
+          source = {{uri: dog}}>
+          <Button
+          onPress={()=>{console.log('LIKED')}}
+          title="Like"
+          color= 'white'
+          accessibilityLabel="'Like' Button"/>
+        </ImageBackground>
        </TouchableOpacity>))}
     </ScrollView>
-    // {tenDogs.map((dog, i) => (<Text key={i}>{i}: {dog}</Text>))}
-    // <Image source={{uri: imageUrl}} />
-
-    // <Image {tenDogs[0]}/>
-
-    // <FlatList
-    //   data={tenDogs}
-    //   renderItem={<Text>{tenDogs}</Text>}
-    //   />
-  // <View>
-  //    {tenDogs.map((dog, i) => { console.log(dog) return (<Image style={{width: 200, height: 200}} source={{uri:"https://images.dog.ceo/breeds/retriever-golden/Z6A_4650_200812.jpg%22%7D%7D/%3E")
-
-  // </View>
   )
 }
 
