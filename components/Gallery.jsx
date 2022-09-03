838422f80a5e10b30dbdebdb4562e7a956596eb0
Irene Wang <ireneziwang@gmail.com>
2022-09-03 14:29:43 -0500

    create new pressable text for liking image in bottom right corner

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, FlatList, ImageBackground, ScrollView, TouchableOpacity, Button} from 'react-native';
+import { Text, View, FlatList, ImageBackground, ScrollView, TouchableOpacity, Pressable, StyleSheet} from 'react-native';
 //TODO diff-ing algorithm that compares previous render with the current code
 //in order for diffing to occur in arrays. react wants us to use KEY PROPS
 //bc it looks at key to determine if list item has changed
@@ -13,18 +13,38 @@ const Gallery = ({tenDogs}) => {
           key={i}
           style={{width: 200, height: 200}}
           source = {{uri: dog}}>
-          <Button
-          onPress={()=>{console.log('LIKED')}}
-          title="Like"
-          color= 'white'
-          accessibilityLabel="'Like' Button"/>
+          <Pressable onPress={()=>{console.log('LIKED')}} style={styles.button}>
+            <Text>LIKE</Text>
+          </Pressable>
         </ImageBackground>
        </TouchableOpacity>))}
     </ScrollView>
   )
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+  },
+  button: {
+    alignSelf: 'flex-end',
+    position: 'absolute',
+    bottom: 25,
+    right: 25
+  }
+});
 
 
 export default Gallery;
